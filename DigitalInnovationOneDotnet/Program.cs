d3e35c6a7a42dd694c7c65d95cd01132e799caa9
CerrutiGH <bielhcsousa@gmail.com>
2021-09-06 09:57:26 -0300

    Create dotnet console App
    Criação de um projeto dotnet console que realiza a leitura dos números dentro de um laço e informa se o número é par ou impar.

---
+++ DigitalInnovationOneDotnet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DigitalInnovationOneDotnet{
+    class Program{
+        static void Main(string[] args){
+
+            int numVezes = 5;
+            for (int i=0; i < numVezes; i++){
+                if(i%2==0){
+                    Console.WriteLine($"O número {i} é par");
+                }
+              else{
+                    Console.WriteLine($"O número {i} é impar");
+              }
+
+            }
+            
+
+        }
+    }
+}
+// See https://aka.ms/new-console-template for more information
