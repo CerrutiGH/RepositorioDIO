f230fa4730938008e73c1d28f3ccff4da12f4d05
CerrutiGH <bielhcsousa@gmail.com>
2021-09-24 09:27:04 -0300

    Create WebAPICatalogoJogos, Controller Jogos and Models

---
+++ WebAPICatalogosJogos/WebAPICatalogosJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPICatalogosJogos.InputModel;
+using WebAPICatalogosJogos.Services;
+using WebAPICatalogosJogos.ViewModel;
+
+namespace WebAPICatalogosJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+
+
+        private readonly IJogoService _jogoService;
+
+        public JogosController(IJogoService jogoService)
+        {
+            _jogoService = jogoService;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5) //Usar async Task para processo assíncrono
+        {
+            var jogos = await _jogoService.Obter(pagina, quantidade);
+            if(jogos.Count() == 0)
+            return NoContent();
+            return Ok(jogos);
+        }
+
+        [HttpGet("{idJogo: guid}")]
+        public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo) 
+        {
+            var jogo = await _jogoService.Obter(idJogo);
+            if (jogo == null)
+                return NoContent();
+            return Ok(jogo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                var jogo = await _jogoService.Inserir(jogoInputModel);
+                return Ok(jogo);
+            }
+            //catch(JogoJaCadastradosException ex)
+            catch(Exception ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para a produtora");
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo( [FromRoute] Guid idJogo, [FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                await _jogoService.Atualizar(idJogo, jogoInputModel);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound("Não e possível atualizar um item que não existe.");
+            }
+            
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo,[FromRoute] double preco)
+        {
+            try
+            {
+                await _jogoService.Atualizar(idJogo, preco);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return NotFound("Não e possível atualizar o preço de um item que não existe.");
+            }
+        }
+
+
+
+
+        [HttpDelete("{idJogo:guid")]
+        public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
+        {
+            try{
+                await _jogoService.Remover(idJogo);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return NotFound("Não e possível deletar um item que não existe.");
+            }
+        }
+    }
+}
---
+++ WebAPICatalogosJogos/WebAPICatalogosJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebAPICatalogosJogos.InputModel
+{
+    public class JogoInputModel
+    {
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 3 e 100 caracteres")]
+        public string Nome {  get; set; }
+
+
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 1 e 100 caracteres")]
+        public string Produtora { get; set; }
+
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "O preço deve ser no mínimo R$1,00 e no máximo R$1000,00")]
+        public double Preco { get; set; }
+    }
+}
